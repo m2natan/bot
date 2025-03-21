8a02a36a37fe9b0bd25ea5788ea1f2e611fbab96
nmesay <nmesay@qena.dev>
2025-03-21 15:22:17 +0300

    updated to fastapi

--- src/main.py
+++ src/main.py
@@ -1,20 +1,7 @@
-from flask import Flask
-import threading
-from telegram.ext import Application
-from credentials import _TOKEN
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=10000)  # Dummy server on port 10000
-
-def main():
-    thread = threading.Thread(target=run_flask)  # Run Flask in a separate thread
-    thread.start()
-
-if __name__ == '__main__':
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Welcome to FastAPI"}
